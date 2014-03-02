dc6e801805e723450fc3734bccea7161d8b5b3db
hovvl <howl@castle.com>
2014-03-02 19:00:50 +0000

    Update Base64.java

--- public/bitlib/src/main/java/com/lambdaworks/crypto/Base64.java
+++ public/bitlib/src/main/java/com/lambdaworks/crypto/Base64.java
@@ -369,14 +369,14 @@ public class Base64
     }
 
 
-    /** decodes a BASE64 encoded byte array that is known to be resonably well formatted. The method is about twice as
+    /** decodes a BASE64 encoded byte array that is known to be reasonably well formatted. The method is about twice as
      * fast as {@link #decode(byte[])}. The preconditions are:<br>
      * + The array must have a line length of 76 chars OR no line separators at all (one line).<br>
      * + Line separator must be "\r\n", as specified in RFC 2045
      * + The array must not contain illegal characters within the encoded string<br>
      * + The array CAN have illegal characters at the beginning and end, those will be dealt with appropriately.<br>
      * @param sArr The source array. Length 0 will return an empty array. <code>null</code> will throw an exception.
-     * @return The decoded array of bytes. May be of length 0.
+     * @return The decoded aray of bytes. May be of length 0.
      */
     public final static byte[] decodeFast(byte[] sArr)
     {
